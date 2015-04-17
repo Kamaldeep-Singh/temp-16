4f04656e271de0a42b37e9875d2a64e9e737eabd
Kamaldeep <kamalmht2000@gmail.com>
2015-04-16 22:28:56 -0700

    delete build & frameworks

--- Crashlytic/build/Crashlytic.build/Release-iphonesimulator/Crashlytic.build/Objects-normal/x86_64/CLLibraryTracker.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/kamaldeep/Desktop/TW_Projects/New/Crashlytic/Crashlytic/Sources/CLLibraryTracker.m \
-  /Users/kamaldeep/Desktop/TW_Projects/New/Crashlytic/Crashlytic/Sources/CLLibraryTracker.h \
-  /Users/kamaldeep/Desktop/TW_Projects/New/Crashlytic/Crashlytic/Sources/CLCoreData.h \
-  /Users/kamaldeep/Desktop/TW_Projects/New/Crashlytic/Crashlytic/Sources/CLDefs.h \
-  /Users/kamaldeep/Desktop/TW_Projects/New/Crashlytic/Crashlytic/Sources/Image.h
